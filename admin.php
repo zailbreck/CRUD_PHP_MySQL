b1806212e674a28047a869ceb1b01d4f812034a0
hagaitrg <disahagait@gmail.com>
2020-03-08 17:10:13 +0700

    Fix data tables bug

--- admin.php
+++ admin.php
@@ -3,13 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Halaman Admin</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"/>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" />
-
     <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" />
    </head>
   <body>
     <nav class="navbar navbar-dark bg-primary mb-3">
@@ -17,10 +16,7 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       </button>
 
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-success" type="submit">Search</button>
-      </form>
+      <a class="navbar-brand" href="#">Admin</a>
     </nav>
 
     <div class="container mb-5">
@@ -39,13 +35,14 @@
                 <th style="text-align : center">AKSI</th>
               </tr>
             </thead>
+            <tbody>
             <?php
             include 'koneksi.php';
             $data = mysqli_query($conn,"SELECT * FROM mahasiswa")or die(mysqli_error());
             $nomor =1 ;
             while ($d = mysqli_fetch_array($data)){
             ?>
-             <tbody>
+             
                 <tr>
                    <td style="text-align : center"><?= $nomor++ ?></td>
                    <td style="text-align : center"><img src="img/<?= $d['gambar']?>" style="width: 100px; height: 100px;"></td>
@@ -55,11 +52,12 @@
                    <td style="text-align : center"><?= $d['jurusan'] ?></td>
                    <td style="text-align : center"><?= $d['angkatan'] ?></td>
                    <td>
-                       <button type="button" name="button" class="btn btn-primary"><a class="edit" href="edit.php?id=<?= $d['id'] ?>" style="color:black">Edit</a></button>
+                       <button type="button" name="button" class="btn btn-info"><a class="edit" href="edit.php?id=<?= $d['id'] ?>" style="color:black">Edit</a></button>
                        <button type="button" name="button" class="btn btn-danger"><a class="hapus"href="delete.php?id=<?= $d['id'] ?>" style="color:black">Delete</a></button>
                 </tr>   
-               </tbody>
+               
            <?php }?>
+            </tbody>
           </table>
       <button type="button" name="button" class="btn btn-success"><a class="tombol" href="input.php" style="color: white">+ Tambah data Baru</a></button>
     </div>
@@ -81,11 +79,11 @@
        }
     } 
      ?>
-    <script>
+  </body> 
+  <script>
       $(document).ready(function(){
         $('#example').DataTable();
       });
 
     </script>
-  </body> 
 </html>
