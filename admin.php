ca237631ab367d27d4f9ac54ed15920eec575415
Muhammad Naufal <avicii.mn@gmail.com>
2020-03-11 19:11:27 +0700

    Update admin.php
    Salah Taro JS

--- admin.php
+++ admin.php
@@ -3,9 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Halaman Admin</title>
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css"/>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" />
@@ -79,6 +77,10 @@
        }
     } 
      ?>
+    <!-- MMQ taro JS aja ga lulus Kampret -->
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
   </body> 
   <script>
       $(document).ready(function(){
